94bf6af423000fcce2e54f81880b9f10a5e3752e
hicklingd <douglashickling2@gmail.com>
2021-02-03 19:01:49 +0000

    ..

--- 34.py
+++ 34.py
@@ -1,35 +1,22 @@
 from itertools import permutations
 from math import factorial
+import math
 from datetime import datetime
 start = datetime.now()
 #9!=362880 so max can be assumed to be 7 digits
 factorials = tuple([factorial(digit) for digit in range(10)])
 sumn=0
+def digits(n):
+	s = 0
+	while n:
+		s += factorials[n%10]
+		n //= 10
+	return s
+
+
 for number5 in range(10,100000):
-    total=0
-    number5_s = [int(nu) for nu in str(number5)]
-    max_num = max(number5_s)
-    digit_c = number5_s.count(max_num)
-    if max_num ==1 and max_num ==2:
-        continue
-    if max_num == 3 and number5>42:
-        continue
-    if max_num == 4 and number5>168:
-        continue
-    if max_num == 5 and number5>840:
-        continue
-    if max_num == 6 and number5>5040:
-        continue
-    if max_num == 7 and number5>35280:
-        continue
-    if max_num == 8 and number5>282240:
-        continue
-    
-
-    
-
-    for digit in number5_s:
-        total+=factorials[digit]
+    total=0 
+    total = digits(number5)
     if total==number5:
         sumn+=number5
 print(sumn)
