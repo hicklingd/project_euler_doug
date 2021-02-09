ca75c3a2269e0874ca4e265812e15ab53b0b0d13
hicklingd <douglashickling2@gmail.com>
2021-02-09 15:40:04 +0000

    ..

--- 39.py
+++ 39.py
@@ -4,7 +4,8 @@ start = datetime.now()
 #a^2+b^2=c^2
 my_dict = dict()
 for a in range(1,500):
-    for b in range(a,500):
+    max_b = 500 if a<250 else 1000-2*a
+    for b in range(a,max_b):
         c=(a**2+b**2)**0.5
         if c%1==0 and a+b+c<=1000:
             if a+b+c in my_dict:
